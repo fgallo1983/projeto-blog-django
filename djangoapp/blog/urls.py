30dcae5c28273b0c13ddfb9dca7f9f85491657db
Felipe Gallo <felipe_gallo@yahoo.com>
2024-09-24 16:00:41 -0300

    Migrando a view tag para ListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 from blog.views import (CategoryListView, CreatedByListView, PostListView,
-                        page, post, search, tag)
+                        TagListView, page, post, search)
 from django.urls import path
 
 app_name = 'blog'
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/',CreatedByListView.as_view(),name='created_by'),
-  path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
     path('search/', search, name='search'),
 ]
